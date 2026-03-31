6a0132e0570a496fb975996b7dd7b0f17970604e
ShaLeeZ2002 <108208048+Shavies@users.noreply.github.com>
2026-03-31 16:04:13 +0700

    make cors *

--- backend-main/src/index.ts
+++ backend-main/src/index.ts
@@ -14,13 +14,8 @@ const PORT = Bun.env.PORT ? parseInt(Bun.env.PORT, 10) : 8080;
 const app = new Elysia()
   .use(
     cors({
-      origin: [
-        "http://localhost:2700",
-        "http://localhost:2701",
-        "https://pea-internship-main.vercel.app",
-        "https://pea-internship-itt.vercel.app"
-      ],
-      credentials: true,
+      origin: "*",
+      credentials: false,
       allowedHeaders: ["Content-Type", "Authorization"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     })
