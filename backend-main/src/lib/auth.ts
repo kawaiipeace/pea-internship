7284b0b83b7b71dbeba07209f9b1128e433ef887
Kittapas004 <6531503004@lamduan.mfu.ac.th>
2026-04-27 14:48:14 +0700

    เพิ่ม ADMIN_EMPLOYEE_IDS

--- backend-main/src/lib/auth.ts
+++ backend-main/src/lib/auth.ts
@@ -9,7 +9,7 @@ type user = InferSelectModel<typeof schema.users>;
 type session = InferSelectModel<typeof schema.sessions>;
 
 const tempStaffData = new Map<string, string>();
-const ADMIN_EMPLOYEE_IDS = ["430792", "492980", "515042", "700003"];
+const ADMIN_EMPLOYEE_IDS = ["430792", "492980", "515042", "700003" , "503710" , "501103" , "515711" ];
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
